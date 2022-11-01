3ee5a68e43fbf015da05a81fbcd270870477f26c
rmo02 <ramoon.maia23@gmail.com>
2022-11-01 11:30:37 -0300

    adicionando skills

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,24 +1,38 @@
 import React from "react";
-import { View, Text,StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export const Home = () => {
   return (
-    <SafeAreaView style={{flex:1,  backgroundColor: "#0C0F14"}}>
-      <View style={{marginTop:10, marginLeft:10, width:'80%'}}>
-        <Text style={styles.text}>Bem-Vindo ao meu portifólio</Text>
-      </View>
-      <View style={{}}>
-      </View>
+    <SafeAreaView style={styles.container}>
+        <View style={styles.intro}>
+          <Text style={styles.text}>Bem-Vindo ao meu portifólio</Text>
+        </View>
     </SafeAreaView>
   );
 };
 
 export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#0C0F14",
+  },
+  intro: {
+    marginTop: 10,
+    marginLeft: 10,
+    width: "80%",
+  },
   text: {
     fontSize: 28,
     color: "#fff",
-    fontWeight:'bold'
+    fontWeight: "bold",
+  },
+  text1:{
+    marginLeft:10,
+    marginTop:10,
+    fontSize: 22,
+    color: "#fff",
+    fontWeight: "400",
   },
 
 });
--- src/pages/stack.tsx
+++ src/pages/stack.tsx
@@ -1,11 +1,208 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, ScrollView, Animated, Image } from 'react-native'
+import React, { useState } from 'react'
+import { FontAwesome5 } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 
  export const StackPage = () => {
+
+  const [progress, setProgress] = useState(new Animated.Value(50));
+  const progressAnim = progress.interpolate({
+      inputRange: [0, 100],
+      outputRange: ['0%','100%']
+  })
+
+  const [progress2, setProgress2] = useState(new Animated.Value(70));
+  const progressAnim2 = progress2.interpolate({
+      inputRange: [0, 100],
+      outputRange: ['0%','100%']
+  })
+
+  const [progress3, setProgress3] = useState(new Animated.Value(50));
+  const progressAnim3 = progress3.interpolate({
+      inputRange: [0, 100],
+      outputRange: ['0%','100%']
+  })
+
+  const [progress4, setProgress4] = useState(new Animated.Value(20));
+  const progressAnim4 = progress4.interpolate({
+      inputRange: [0, 100],
+      outputRange: ['0%','100%']
+  })
+
+  const [progress5, setProgress5] = useState(new Animated.Value(80));
+  const progressAnim5 = progress5.interpolate({
+      inputRange: [0, 100],
+      outputRange: ['0%','100%']
+  })
+
   return (
-    <View>
-      <Text>StackPage</Text>
+    <View style={styles.container}>
+      <View style={styles.intro}>
+          <Text style={styles.text}>Skills</Text>
+      </View>
+
+      <ScrollView>
+        <View style={styles.cards}>
+        <View style={styles.card}>
+          <View style={{flexDirection:'row', alignItems:'center', justifyContent:'space-around', marginTop:30}}>
+          <FontAwesome5 name="react" size={60} color="#2AB3FF" />
+          <View style={{flexDirection: 'column', justifyContent:'space-around', width:'70%'}}>
+          <Text style={{fontSize:20, fontWeight:'bold', color:'#2AB3FF'}}>React Js</Text>
+          <View>
+          <View style={{width: '100%',
+            height: 20,
+            borderRadius: 30, marginTop:20, backgroundColor: '#2AB3FF' + 30}}>
+            <Animated.View style={[{
+                height: 20,
+                borderRadius: 20,
+                backgroundColor:'#2AB3FF', alignItems:'center'
+                },{
+                    width: progressAnim
+                }]}>
+                <Text style={{color:"#fff"}}>50%</Text>
+            </Animated.View>
+            </View>
+          </View>
+          </View>
+          </View>
+        </View>
+
+        <View style={styles.card}>
+          <View style={{flexDirection:'row', alignItems:'center', justifyContent:'space-around', marginTop:30}}>
+          <FontAwesome5 name="react" size={60} color="#173796" />
+          <View style={{flexDirection: 'column', justifyContent:'space-around', width:'70%'}}>
+          <Text style={{fontSize:20, fontWeight:'bold', color:'#173796'}}>React Native</Text>
+          <View>
+          <View style={{width: '100%',
+            height: 20,
+            borderRadius: 30, marginTop:20, backgroundColor: '#173796' + 30}}>
+            <Animated.View style={[{
+                height: 20,
+                borderRadius: 20,
+                backgroundColor:'#173796', alignItems:'center'
+                },{
+                    width: progressAnim2
+                }]}>
+                <Text style={{color:"#fff"}}>70%</Text>
+            </Animated.View>
+            </View>
+          </View>
+          </View>
+          </View>
+        </View>
+
+        <View style={styles.card}>
+          <View style={{flexDirection:'row', alignItems:'center', justifyContent:'space-around', marginTop:30}}>
+          <Image 
+            style={{height:90, width:60}} 
+            resizeMode="contain" 
+            source={require("../../assets/flutter-logo.png")} 
+            />
+          <View style={{flexDirection: 'column', justifyContent:'space-around', width:'70%'}}>
+          <Text style={{fontSize:20, fontWeight:'bold', color:'#54C5F8'}}>Flutter</Text>
+          <View>
+          <View style={{width: '100%',
+            height: 20,
+            borderRadius: 30, marginTop:20, backgroundColor: '#54C5F8' + 30}}>
+            <Animated.View style={[{
+                height: 20,
+                borderRadius: 20,
+                backgroundColor:'#54C5F8', alignItems:'center'
+                },{
+                    width: progressAnim3
+                }]}>
+                <Text style={{color:"#fff"}}>50%</Text>
+            </Animated.View>
+            </View>
+          </View>
+          </View>
+          </View>
+        </View>
+
+        <View style={styles.card}>
+          <View style={{flexDirection:'row', alignItems:'center', justifyContent:'space-around', marginTop:30}}>
+          <MaterialCommunityIcons name="language-typescript" size={45} color="#2F72BC" />
+          <View style={{flexDirection: 'column', justifyContent:'space-around', width:'70%'}}>
+          <Text style={{fontSize:20, fontWeight:'bold', color:'#2F72BC'}}>TypeScript</Text>
+          <View>
+          <View style={{width: '100%',
+            height: 20,
+            borderRadius: 30, marginTop:20, backgroundColor: '#2F72BC' + 30}}>
+            <Animated.View style={[{
+                height: 20,
+                borderRadius: 20,
+                backgroundColor:'#2F72BC', alignItems:'center'
+                },{
+                    width: progressAnim4
+                }]}>
+                <Text style={{color:"#fff"}}>20%</Text>
+            </Animated.View>
+            </View>
+          </View>
+          </View>
+          </View>
+        </View>
+
+        <View style={styles.card}>
+          <View style={{flexDirection:'row', alignItems:'center', justifyContent:'space-around', marginTop:30}}>
+          <Ionicons name="logo-javascript" size={45} color="#EAD41C" />
+          <View style={{flexDirection: 'column', justifyContent:'space-around', width:'70%'}}>
+          <Text style={{fontSize:20, fontWeight:'bold', color:'#EAD41C'}}>JavaScript</Text>
+          <View>
+          <View style={{width: '100%',
+            height: 20,
+            borderRadius: 30, marginTop:20, backgroundColor: '#EAD41C' + 30}}>
+            <Animated.View style={[{
+                height: 20,
+                borderRadius: 20,
+                backgroundColor:'#EAD41C', alignItems:'center'
+                },{
+                    width: progressAnim5
+                }]}>
+                <Text style={{color:"#fff"}}>80%</Text>
+            </Animated.View>
+            </View>
+          </View>
+          </View>
+          </View>
+        </View>
+
+
+        </View>
+      </ScrollView>
     </View>
   )
 }
 
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#0C0F14",
+  },
+  intro: {
+    marginTop: 30,
+    marginLeft: 10,
+    width: "80%",
+  },
+  text: {
+    fontSize: 28,
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  cards:{
+    marginTop:10,
+    alignItems:'center'
+  },
+ card: {
+  flexWrap:'wrap',
+  width:"90%",
+  paddingHorizontal: 10,
+  height: 130,
+  backgroundColor: "white",
+  marginBottom: 15,
+  borderRadius: 10,
+  elevation:5
+},
+
+});
