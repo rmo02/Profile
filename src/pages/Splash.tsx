4985b00938de31c2f38717c47d1c5b4fa1d4c7d0
rmo02 <ramoon.maia23@gmmail.com>
2022-11-07 21:17:37 -0300

    melhorias

--- src/pages/Splash.tsx
+++ src/pages/Splash.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, Image, Linking,  TouchableOpacity } from "react-native";
 import LottieView from 'lottie-react-native';
 import { useNavigation } from "@react-navigation/native";
@@ -7,13 +7,22 @@ import { propsStack } from "../routes/Models";
 export const Splash = () => {
   const navigation = useNavigation<propsStack>();
 
+  const [loop, setLoop] = useState(true);
+
+  useEffect(
+    () => {
+      let timer = setTimeout(() => setLoop(false), 50000);
+      return () => {
+        clearTimeout(timer);
+      };
+    },[]);
+
   return (
     <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
         <LottieView source={require('../../assets/dev.json')}
          style={{backgroundColor:'#051428'}}
          autoPlay
-         duration={3}
-         loop={false}
+         loop={loop}
          onAnimationFinish={()=> navigation.navigate('Home')}
          />
     </View>
